7681267a1748e9c341d9744d2e0e6fecbe4f337d
Hassaanjbaig-code <hassaanb000@gmail.com>
2023-08-02 00:54:06 +0500

    FInal commit every thing is workign

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    include ActiveModel::SecurePassword
+    has_secure_password
     has_many :product
     has_many :cart
+
+    # attr_accessor :password_digest
 end
