9193f4500ab625119cfe86beb5b18b4fb8ebea5a
Hassaanjbaig-code <hassaanb000@gmail.com>
2023-08-02 02:28:28 +0500

    Genrate a token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,25 +1,40 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:create]
+  before_action :set_user, only: [:show, :destroy]
+
   def index
-    render json: User.all
+    render json: User.all, status: :ok
   end
 
   def create
-    user = User.create(user_params)
+    user = User.new(user_params)
     if user.save
       render json: user, status: :created, notice: 'User is created'
       else
-        render json: { error: 'User is not created' }, status: :bad_request
+        render json: { error: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  # def logged_in
+  #   user = user.find(params[:email])
+  # end
 
   def show
     user = User.find(params[:id])
     render json: user
   end
 
+  def destroy
+    @user.destroy
+  end
+
   private
 
   def user_params
     params.permit(:name, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [ :index, :create, :show ]
+      post '/login', to: 'authentications#login'
       resources :products, only: [ :index, :show, :create ]
       resources :carts, only: [:index, :show, :create, :destroy]
     end
