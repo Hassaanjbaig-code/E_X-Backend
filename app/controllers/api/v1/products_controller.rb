5c8af4245223186e8c751850ef1c49a5afc87028
Hassaanjbaig-code <hassaanb000@gmail.com>
2023-08-03 02:21:31 +0500

    Add update for the products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProductsController < ApplicationController
     skip_before_action :authenticate_request, only: [:index, :show ]
-    before_action :set_user, only: [:create, :destroy, :myproduct]
+    before_action :set_user, only: [:create, :destroy, :myproduct, :update]
     def index 
         product = Product.all.order(created_at: :desc)
         render json: product
@@ -43,6 +43,18 @@ class Api::V1::ProductsController < ApplicationController
           render json: @product, status: :created, notice: 'Product is created'
         else
           render json: { error: 'Product is not created' }, status: :bad_request
+        end
+      end
+
+      def update
+        @user = current_user
+        @product = @user.product.find(params[:id])
+        @product.user_id = @user.id
+
+        if @product.update(product_params)
+          render json: @product , notice: 'Product is updated', status: :created
+        else
+          render json: { error: 'Something went wrong, Could not update the products '}, status: :bad_request 
         end
       end
       
